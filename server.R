f8bdb9f4d3b033288f74b0baf7449cb0f2e54ca1
Virginia McGhee <mcgheev19@mail.wlu.edu>
2017-10-19 12:54:48 -0400

    used iucn.category instead of iucn

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ function(input,output){
   output$countryPlot <- renderPlot({
     worldData%>%
       filter(country == input$country) %>%
-      ggplot(aes(species, value, fill = iucn)) + geom_bar( stat = "identity")
+      ggplot(aes(species, value, fill = iucn.category)) + geom_bar( stat = "identity")
       
   })
   
