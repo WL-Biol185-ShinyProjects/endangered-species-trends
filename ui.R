cb1990567234d7aa3cece3c2318bf9eefd86858d
Emily Limmer <limmere18@mail.wlu.edu>
2017-10-24 12:21:58 -0400

    Works with tabs now

--- ui.R
+++ ui.R
@@ -2,25 +2,33 @@ library(shiny)
 
 worldData <- read.table("worldData.txt")
 
+
 # Define UI for application that draws a histogram
 fluidPage(
   
-  # Application title
-  titlePanel("Species Status by Country"),
+  navbarPage("Endangeres Species App",
+             tabPanel("Country Bar Graph", # Application title
+             titlePanel("Species Status by Country"),
+             
+             # Sidebar with a slider input for number of bins 
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(inputId = "country",
+                             label = "Select a Country", 
+                             choices = unique(worldData$country)
+                 )),
+               
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("countryPlot")))) ,
+            
+            tabPanel("Global Heat Map"),
+             tabPanel("U.S. Map")
+  ))
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "country",
-                 label = "Select a Country", 
-                 choices = unique(worldData$country)
-                  )
-         ),
-                   
+
+  
+ 
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("countryPlot")
-    )
-  )
-)
+  
