1d0a958c01f0edead7e5be30791b168d96e4708d
Olivia Kubli <kublio18@mail.wlu.edu>
2017-11-30 13:14:04 -0500

    Added links on the source page

--- ui.R
+++ ui.R
@@ -28,7 +28,17 @@ navbarPage( "Endangered Species App"
             
           , globalTab
           
-          , tabPanel( "Sources")
+          , tabPanel( "Sources",
+                      
+                      titlePanel("Data Sources"),
+                      
+                      br(),
+                      
+                      
+                      helpText( a("U.S. FWS Threatened & Endangered Species", href = "https://ecos.fws.gov/ecp/report/table/critical-habitat.html", target = "_blank")),
+                      helpText( a("OECD Threatended Species by Country", href = "https://ecos.fws.gov/ecp/report/table/critical-habitat.html", target = "_blank"))
+                    )
           )
+          
 
 
