26af199bc755bb03e2125e5c254126c8357bd76f
Olivia Kubli <kublio18@mail.wlu.edu>
2017-12-05 13:06:39 -0500

    Added Geojson data source on source page

--- sourcesPage.R
+++ sourcesPage.R
@@ -6,5 +6,11 @@ sourcesPage <- tabPanel( "Sources",
           
           
           helpText( a("U.S. FWS Threatened & Endangered Species", href = "https://ecos.fws.gov/ecp/report/table/critical-habitat.html", target = "_blank")),
-          helpText( a("OECD Threatended Species by Country", href = "https://ecos.fws.gov/ecp/report/table/critical-habitat.html", target = "_blank"))
-)
+          helpText( a("OECD Threatended Species by Country", href = "https://ecos.fws.gov/ecp/report/table/critical-habitat.html", target = "_blank")),
+          
+          br(),
+        
+          h5("GeoJSON border data:",
+             helpText( a("Johan Sundström GitHub", href="https://github.com/johan/world.geo.json/blob/master/countries.geo.json", target="_blank"))
+                )
+            )
